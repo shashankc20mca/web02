cf78f3b8fdbca74463dc61a2a11f276e84757b28
shashank c <120564691+shashankc20mca@users.noreply.github.com>
2026-01-10 18:19:30 +0530

    Update enable_httpd.sh

--- enable_httpd.sh
+++ enable_httpd.sh
@@ -1,1 +1,1 @@
-docker run --name tomcat -p 8081:8080 -d tomcat:9.0.113-jdk8-corretto-al2
+docker run --name tomcat1 -p 8081:8080 -v myvolume:/usr/local/tomcat/webapps/ROOT/ -d tomcat:9.0.113-jdk8-corretto-al2
