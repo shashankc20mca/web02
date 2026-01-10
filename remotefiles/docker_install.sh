66f7cb674e6a63aaee3da209adde2ce0eec321e3
shashank c <120564691+shashankc20mca@users.noreply.github.com>
2026-01-10 16:01:38 +0530

    Create docker_install.sh

---
+++ remotefiles/docker_install.sh
@@ -0,0 +1,16 @@
+sudo yum remove -y docker \
+  docker-client \
+  docker-client-latest \
+  docker-common \
+  docker-latest \
+  docker-latest-logrotate \
+  docker-logrotate \
+  docker-engine
+sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo docker run hello-world
+sudo groupadd docker 
+sudo usermod -aG docker vagrant
